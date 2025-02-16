2469a2538b5df73a8169c266da624fc947fc6f5a
nachum <nahum0583@gmail.com>
2025-02-16 11:23:13 +0200

    fwriter

--- engin/file_writer.py
+++ engin/file_writer.py
@@ -8,9 +8,19 @@ class FileWriter:
         self.data = data
         self.writing()
 
-    def writing(self):
-        file_name = os.path.join('log_data')
-        data = {str(self.data[-1]): self.data[:-2]}
-        with open(file_name, 'a') as f:
-            json.dump(data, f, indent=2)
+    def writing(self):       
+        file_name = os.path.join(f'log_data {self.data[-1]}.json')
+        
+        try:
+           with open(file_name, "r") as f:
+              data = json.load(f)
 
+        except :
+           data = {}
+
+        key = str(self.data[-2])
+        value = self.data[:-2]
+        data[key] = value
+
+        with open(file_name, "w") as f:
+            json.dump(data, f, indent=2)
