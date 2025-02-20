5f437cacf7cb9117620656cde54d72ac3fc7688e
YosefGu <yos770yos@gmail.com>
2025-02-20 10:12:08 +0200

    Creating a server

---
+++ backend/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from routes import routs
+
+app = Flask(__name__)
+
+app.register_blueprint(routs)
+
+if __name__ == '__main__':
+    
+    app.run(debug=True, port=5000)
+
