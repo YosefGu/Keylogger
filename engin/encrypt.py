145d7fd6bfe3d2314c9750a35c2757743773d794
RudeusTempest <yosefkaro2003@gmail.com>
2025-02-25 16:13:18 +0200

    updated encryption file

--- engin/encrypt.py
+++ engin/encrypt.py
@@ -1,17 +1,19 @@
 class Encryption:
-    def _init_(self):
+    
+    def __init__(self,data):
         self.password = 42
+        self.data = data
 
-    def xor_encryption(self, data):
-        xored_string = ""
-        for i, char in enumerate(data):
-            xored_character = ord(char) ^ self.password
-            xored_string += chr(xored_character)
-        return xored_string
-
-    def xor_decryption(self, data):
-        xored_string = ""
-        for i, char in enumerate(data):
-            xored_character = ord(char) ^ self.password
-            xored_string += chr(xored_character)
-        return xored_string
+    def xor_encryption(self):
+        xored_array = []
+        for word in (self.data):
+            if len(word)>1:
+                for letter in word:
+                    xored_character = ord(letter) ^ self.password
+                    xored_array.append(chr(xored_character))
+            elif word == " " :
+                xored_array += " " 
+            else:         
+                xored_character = ord(word) ^ self.password
+                xored_array += chr(xored_character)     
+        return xored_array
